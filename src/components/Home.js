64399aac7a618e5c6692ab97a9e636b2e85a9ce9
ido evergreen <dicksonevergreenido@gmail.com>
2021-05-21 12:32:51 +0100

    add axios config files

---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React,{useEffect} from 'react'
+
+import axios from 'axiso'
+
+export default function Home() {
+    return (
+        <div>
+            
+useEffect(() => {
+
+
+axios.get('Products')
+
+    
+   
+}, [])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
